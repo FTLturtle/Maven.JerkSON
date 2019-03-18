173ffdc7aae1cde3eb89d0176f64f91fc8755604
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-18 12:20:01 -0400

    refactored GroceryReporter

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -9,86 +9,76 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class GroceryReporter {
-    private final String originalFileText;
     private Map<String, List<Item>> itemMap;
     private int numberOfErrors;
 
     public GroceryReporter(String jerksonFileName) {
-        this.originalFileText = FileReader.readFile(jerksonFileName);
+        String originalFileText = FileReader.readFile(jerksonFileName);
         ItemParser itemParser = new ItemParser();
-        List<Item> itemList = itemParser.parseItemList(originalFileText);
         itemMap = new TreeMap<>(new GroceryComparator());
+        populateItemMap(originalFileText, itemParser);
+        numberOfErrors = itemParser.getNumberOfErrors();
+    }
 
+    private void populateItemMap(String originalFileText, ItemParser itemParser) {
+        List<Item> itemList = itemParser.parseItemList(originalFileText);
         List<Item> items;
         for (Item item : itemList) {
             items = itemMap.getOrDefault(item.getName(), new ArrayList<>());
             items.add(item);
             itemMap.put(item.getName(), items);
         }
-
-        numberOfErrors = itemParser.getNumberOfErrors();
     }
 
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        int numberOfItems;
-        int numberOfItemsAtPrice = 0;
-        Map<Double, Integer> itemPriceMap;
+        return extractStringOutputFromMap();
+    }
 
+    private String extractStringOutputFromMap() {
+        StringBuilder output = new StringBuilder();
         for (String key : itemMap.keySet()) {
-            numberOfItems = itemMap.get(key).size();
-            String keyUpperCase = key.substring(0,1).toUpperCase() + key.substring(1);
-            stringBuilder.append(String.format("name:%8s \t\t seen: %d times\n", keyUpperCase, numberOfItems))
-                    .append("============= \t \t =============\n");
-
-            itemPriceMap = new TreeMap<>(new PriceComparator());
-            for (Item item : itemMap.get(key)) {
-                numberOfItemsAtPrice = itemPriceMap.getOrDefault(item.getPrice(), 0);
-                itemPriceMap.put(item.getPrice(), numberOfItemsAtPrice + 1);
-            }
-
-            for (Double price : itemPriceMap.keySet()) {
-                numberOfItemsAtPrice = itemPriceMap.get(price);
-                stringBuilder.append(String.format("Price: \t %4.2f\t\t seen: %d times\n", price, numberOfItemsAtPrice))
-                .append("-------------\t\t -------------\n");
-            }
-            if (numberOfItemsAtPrice == 1) {
-                stringBuilder.replace(stringBuilder.length() - 32, stringBuilder.length(), "\n");
-            }
-            stringBuilder.append("\n");
+            appendItemString(output, key);
+            appendAllPriceStringsForItem(output, key);
         }
-
-        stringBuilder.replace(stringBuilder.length() - 31, stringBuilder.length(), "");
-
-        stringBuilder.append(String.format("\nErrors         \t \t seen: %d times\n", numberOfErrors));
-
-        return stringBuilder.toString();
+        output.replace(output.length() - 31, output.length(), "");
+        output.append(String.format("\nErrors         \t \t seen: %d times\n", numberOfErrors));
+        return output.toString();
     }
 
-    public static void main(String[] args) {
-        String string = "name:    Milk \t\t seen: 6 times\n" +
-                "============= \t \t =============\n" +
-                "Price: \t 3.23\t\t seen: 5 times\n" +
-                "-------------\t\t -------------\n" +
-                "Price:   1.23\t\t seen: 1 time\n" +
-                "\n" +
-                "name:   Bread\t\t seen: 6 times\n" +
-                "=============\t\t =============\n" +
-                "Price:   1.23\t\t seen: 6 times\n" +
-                "-------------\t\t -------------\n" +
-                "\n" +
-                "name: Cookies     \t seen: 8 times\n" +
-                "=============     \t =============\n" +
-                "Price:   2.25        seen: 8 times\n" +
-                "-------------        -------------\n" +
-                "\n" +
-                "name:  Apples     \t seen: 4 times\n" +
-                "=============     \t =============\n" +
-                "Price:   0.25     \t seen: 2 times\n" +
-                "-------------     \t -------------\n" +
-                "Price:   0.23  \t \t seen: 2 times\n" +
-                "\n" +
-                "Errors         \t \t seen: 4 times\n";
+    private void appendItemString(StringBuilder output, String key) {
+        int numberOfItems;
+        numberOfItems = itemMap.get(key).size();
+        String keyUpperCase = key.substring(0,1).toUpperCase() + key.substring(1);
+        output.append(String.format("name:%8s \t\t seen: %d times\n", keyUpperCase, numberOfItems))
+                .append("============= \t \t =============\n");
     }
+
+    private void appendAllPriceStringsForItem(StringBuilder output, String key) {
+        Map<Double, Integer> itemPriceMap = new TreeMap<>(new PriceComparator());
+        populateItemPriceMap(key, itemPriceMap);
+        appendPriceString(output, itemPriceMap);
+        output.append("\n");
+    }
+
+    private void populateItemPriceMap(String key, Map<Double, Integer> itemPriceMap) {
+        int numberOfItemsAtPrice;
+        for (Item item : itemMap.get(key)) {
+            numberOfItemsAtPrice = itemPriceMap.getOrDefault(item.getPrice(), 0);
+            itemPriceMap.put(item.getPrice(), numberOfItemsAtPrice + 1);
+        }
+    }
+
+    private void appendPriceString(StringBuilder output, Map<Double, Integer> itemPriceMap) {
+        int numberOfItemsAtPrice = 0;
+        for (Double price : itemPriceMap.keySet()) {
+            numberOfItemsAtPrice = itemPriceMap.get(price);
+            output.append(String.format("Price: \t %4.2f\t\t seen: %d times\n", price, numberOfItemsAtPrice))
+            .append("-------------\t\t -------------\n");
+        }
+        if (numberOfItemsAtPrice == 1) {
+            output.replace(output.length() - 32, output.length(), "\n");
+        }
+    }
+    
 }
